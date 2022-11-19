de849041219ff6485db3a2eb5aecdc4ca6d1b22b
arda cansizoglu <ardacansizoglu@gmail.com>
2022-11-19 17:05:38 +0100

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  # get 'tasks', to: 'tasks#index'
-  # get 'tasks/new', to: 'tasks#new'
-  # get 'tasks/:id', to: 'tasks#show', as: :task
-  # post 'tasks', to: 'tasks#create'
-  # get 'tasks/:id/edit', to: 'tasks#edit'
-  # patch 'tasks/:id', to: 'tasks#update'
-  # delete 'tasks/:id', to: 'tasks#destroy'
-  resources :tasks
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
